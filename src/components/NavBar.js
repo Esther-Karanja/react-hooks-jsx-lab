999cabc9005df4db129ece37bb3423546b13abcf
Esther <est.karanja@gmail.com>
2023-08-21 18:41:57 -0700

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+  <div id="Nav">
+    <nav>
+    <a href="#Home">Home</a>
+    <a href="#About">About</a>
+  </nav>
+  </div>
+  );
 }
 
 export default NavBar;
